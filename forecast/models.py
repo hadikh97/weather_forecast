05a049412020bb55a6eecb7d0b0031c0c0c3df14
Hadi <hadi.khashei.1376@gmail.com>
2024-06-19 04:55:01 +0330

    making database

--- forecast/models.py
+++ forecast/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Weather(models.Model):
+    country = models.CharField(max_length=100)
+    date = models.DateField()
+    day = models.CharField(max_length=10)
+    condition = models.CharField(max_length=100)
+    icon = models.CharField(max_length=200)
+    min_temp = models.FloatField()
+    max_temp = models.FloatField()
+
+    class Meta:
+        unique_together = ('country', 'date')
+
+    def __str__(self):
+        return f"{self.country} - {self.date}"
--- forecast/views.py
+++ forecast/views.py
@@ -1,34 +1,65 @@
 import requests
+from django.conf import settings
 from django.shortcuts import render
 from datetime import datetime, timedelta
+from .models import Weather
 
-API_KEY = 'c53bb4104eb941a6b49144006241806'
+API_KEY = 'fc20461f33ae4e958b812325241906'
 API_URL = 'https://api.weatherapi.com/v1'
 
 
+def fetch_weather_data(country, date):
+    response = requests.get(f'{API_URL}/forecast.json?key={API_KEY}&q={country}&dt={date.strftime("%Y-%m-%d")}')
+    if response.status_code == 200:
+        data = response.json()
+        forecast = data.get('forecast', {}).get('forecastday', [])[0]
+        return {
+            'day': date.strftime('%A'),
+            'condition': forecast.get('day', {}).get('condition', {}).get('text', ''),
+            'icon': forecast.get('day', {}).get('condition', {}).get('icon', ''),
+            'min_temp': forecast.get('day', {}).get('mintemp_c', ''),
+            'max_temp': forecast.get('day', {}).get('maxtemp_c', '')
+        }
+    else:
+        return None
+
+
 def get_weather_data(country):
-    today = datetime.today()
+    today = datetime.today().date()
     dates = {
-        'yesterday': (today - timedelta(days=1)),
+        'yesterday': today - timedelta(days=1),
         'today': today,
-        'tomorrow': (today + timedelta(days=1))
+        'tomorrow': today + timedelta(days=1)
     }
     weather_data = {}
+
     for day, date in dates.items():
-        response = requests.get(f'{API_URL}/forecast.json?key={API_KEY}&q={country}&dt={date.strftime("%Y-%m-%d")}')
-        if response.status_code == 200:
-            data = response.json()
-            forecast = data.get('forecast', {}).get('forecastday', [])[0]
+        weather = Weather.objects.filter(country=country, date=date).first()
+
+        if not weather:
+            data = fetch_weather_data(country, date)
+            if data:
+                weather = Weather(
+                    country=country,
+                    date=date,
+                    day=data['day'],
+                    condition=data['condition'],
+                    icon=data['icon'],
+                    min_temp=data['min_temp'],
+                    max_temp=data['max_temp']
+                )
+                weather.save()
+        if weather:
             weather_data[day] = {
-                'day': date.strftime('%A'),  # Day of the week
-                'condition': forecast.get('day', {}).get('condition', {}).get('text', ''),
-                'icon': forecast.get('day', {}).get('condition', {}).get('icon', ''),
-                'min_temp': forecast.get('day', {}).get('mintemp_c', ''),
-                'max_temp': forecast.get('day', {}).get('maxtemp_c', '')
+                'day': weather.day,
+                'condition': weather.condition,
+                'icon': weather.icon,
+                'min_temp': weather.min_temp,
+                'max_temp': weather.max_temp
             }
         else:
             weather_data[day] = {
-                'error': response.json().get('error', {}).get('message', 'Unknown error')
+                'error': 'Unable to fetch weather data'
             }
     return weather_data
 
